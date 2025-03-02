a940a96e1fc180bb2412cbb72733477f579b433c
Subodh Rana <subodhrana390@gmail.com>
2025-03-03 00:55:34 +0530

    fix

--- frontend/src/Dashboard/AdminDashboard.jsx
+++ frontend/src/Dashboard/AdminDashboard.jsx
@@ -16,7 +16,7 @@ import Profile from "./components/Profile/Profile";
 import ReservationList from "./components/reservation/ReservationList";
 import ViewReservation from "./components/reservation/component/ViewReservation";
 import CustomerList from "./components/Customer/CustomerList";
-import ViewCustomer from "./components/Customer/ViewCustomeriewCustomer";
+import ViewCustomer from "./components/Customer/ViewCustomer";
 
 const AdminDashboard = () => {
   return (
